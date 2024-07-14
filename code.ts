29e0070abd4d3e6d9c00b36740e25e5aa4ab146f
sthira-conicle <sthira@conicle.com>
2024-07-15 01:13:21 +0700

    upload

---
+++ code.ts
@@ -0,0 +1,85 @@
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+/*figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-rectangles') {
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};*/
+
+figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'round-border') {
+    const selection = figma.currentPage.selection;
+    for (const node of selection) {
+      if ('cornerRadius' in node) {
+        const width = node.width;
+        const height = node.height;
+        switch (true) {
+          case (Math.min(width, height) <= 6) :
+            node.cornerRadius = 0;
+            break;
+          case (Math.min(width, height) <= 8) :
+            node.cornerRadius = 2;
+            break;
+          case (Math.min(width, height) <= 16) :
+            node.cornerRadius = 4;
+            break;
+          case (Math.min(width, height) <= 24) :
+            node.cornerRadius = 6;
+            break;
+          case (Math.min(width, height) <= 40) :
+            node.cornerRadius = 8;
+            break;
+          case (Math.min(width, height) <= 64) :
+            node.cornerRadius = 12;
+            break;
+          case (Math.min(width, height) <= 96) :
+            node.cornerRadius = 16;
+            break;
+          case (Math.min(width, height) <= 192) :
+            node.cornerRadius = 20;
+            break;
+          case (Math.min(width, height) <= 320) :
+            node.cornerRadius = 24;
+            break;
+          default:
+              node.cornerRadius = 32;
+              break;
+          }
+      }
+    }
+    figma.viewport.scrollAndZoomIntoView(selection);
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
