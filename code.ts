779c24aaa869da5f5b0f1c53a8b4a6341d8c031f
sthira-conicle <sthira@conicle.com>
2024-07-15 04:56:09 +0700

    update1

--- code.ts
+++ code.ts
@@ -7,33 +7,12 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(
+  __html__,
+  { width: 300, height: 320, title: "Corner rounder" }
+)
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-/*figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};*/
-
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+figma.ui.onmessage =  (msg: {type: string, style: string}) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
   if (msg.type === 'round-border') {
@@ -42,6 +21,7 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
       if ('cornerRadius' in node) {
         const width = node.width;
         const height = node.height;
+    if (msg.style === 'user') {
         switch (true) {
           case (Math.min(width, height) <= 6) :
             node.cornerRadius = 0;
@@ -74,12 +54,41 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
               node.cornerRadius = 32;
               break;
           }
+        } else if (msg.style === 'admin')
+          {
+            switch (true) {
+              case (Math.min(width, height) <= 6) :
+                node.cornerRadius = 0;
+                break;
+              case (Math.min(width, height) <= 8) :
+                node.cornerRadius = 2;
+                break;
+              case (Math.min(width, height) <= 16) :
+                node.cornerRadius = 4;
+                break;
+              case (Math.min(width, height) <= 24) :
+                node.cornerRadius = 6;
+                break;
+              case (Math.min(width, height) <= 40) :
+                node.cornerRadius = 8;
+                break;
+              case (Math.min(width, height) <= 128) :
+                node.cornerRadius = 12;
+                break;
+              case (Math.min(width, height) <= 320) :
+                node.cornerRadius = 16;
+                break;
+              default:
+                  node.cornerRadius = 20;
+                  break;
+              }
+          }
       }
     }
+  
     figma.viewport.scrollAndZoomIntoView(selection);
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  if (msg.type === 'close') {
+    figma.closePlugin();
+  }
 };
